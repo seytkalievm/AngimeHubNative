48abba3349897487a8e36a0aaf026360e9d7bd57
Makshe Seytkaliev <3meuka0@gmail.com>
2022-06-20 21:45:57 +0300

    Created cardviews for show preview and artists. Created Profile fragment

--- app/src/main/java/com/seytkalievm/angimehubnative/ui/main/SessionActivity.kt
+++ app/src/main/java/com/seytkalievm/angimehubnative/ui/main/SessionActivity.kt
@@ -2,6 +2,9 @@ package com.seytkalievm.angimehubnative.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -9,6 +12,8 @@ import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.seytkalievm.angimehubnative.R
 import com.seytkalievm.angimehubnative.databinding.ActivitySessionBinding
+
+const val TAG = "SessionActivity"
 
 class SessionActivity : AppCompatActivity() {
 
@@ -22,12 +27,52 @@ class SessionActivity : AppCompatActivity() {
 
         binding = ActivitySessionBinding.inflate(layoutInflater).apply {
             toolBar = mainActivityToolbar
-            bottomNavBar = bottomNavMenu
             toolBar.setTitle(R.string.stand_up_shows)
             setSupportActionBar(toolBar)
+            bottomNavBar = bottomNavMenu
             setContentView(root)
         }
-        navController = Navigation.findNavController(this, R.id.main_activity_host_fragment)
+        navController = Navigation.findNavController(this, R.id.session_activity_host_fragment)
         NavigationUI.setupWithNavController(bottomNavBar, navController)
+
+        bottomNavBar.setOnItemReselectedListener { item ->
+            val reselectedDestinationId = item.itemId
+            navController.popBackStack(reselectedDestinationId, inclusive = false)
+        }
+
+        bottomNavBar.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navController)
+            navController.popBackStack(item.itemId, false)
+            true
+        }
+
+        binding.sessionShowProfileIb.setOnClickListener { showProfile() }
     }
+
+    override fun onBackPressed() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        bottomNavBar.visibility = VISIBLE
+        super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun BottomNavigationView.uncheckAllItems() {
+        menu.setGroupCheckable(0, true, false)
+        for (i in 0 until menu.size()) {
+            menu.getItem(i).isChecked = false
+        }
+        menu.setGroupCheckable(0, true, true)
+    }
+
+    private fun showProfile(){
+        toolBar.setTitle(R.string.profile)
+        bottomNavBar.uncheckAllItems()
+        navController.navigate(R.id.profileFragment)
+    }
+
+
 }
